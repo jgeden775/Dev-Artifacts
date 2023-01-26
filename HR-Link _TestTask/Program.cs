545fde2ef984ac7335040aca98661c3213bd5b14
John <jeden667@gmail.com>
2023-01-26 16:47:42 +0300

    Initial commit. Add 2 folders

---
+++ HR-Link _TestTask/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+int TotalApartPrice(int N, int M, int X)
+{
+    int totalPrc = 0;
+    int stgCount = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        totalPrc += X;
+        stgCount++;
+
+        if (stgCount == M)
+        {
+            X += 1000;
+            stgCount = 0;
+        }
+    }
+    return totalPrc;
+}
+
+Console.WriteLine($"Total Price: {TotalApartPrice(40, 10, 10000)}$");
